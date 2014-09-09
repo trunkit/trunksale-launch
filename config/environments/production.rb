19403961f1463ef883c95e6fd5039dc8c2b90e82
sfelsey <sfelsey@gmail.com>
2014-09-09 00:11:59 -0600

    added another config asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Prelaunchr::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.assets.precompile += %w( core.css bootstrap.css reset.css active_admin.css active_admin/print.css )
+  config.assets.precompile += %w( core.css bootstrap.css reset.css active_admin.css active_admin/print.css active_admin.js )
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
