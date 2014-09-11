5e9f27dd1c8ad41157295ebcbb0589870c5f3d48
William Weidendorf <wweidendorf@gmail.com>
2014-09-11 01:20:26 -0400

    Don't url_for just a path in a mailer view

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Prelaunchr::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'www.thetrunksale.com' }
+  config.action_mailer.default_url_options = { host: 'www.thetrunksale.com', only_path: false }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings   = {
