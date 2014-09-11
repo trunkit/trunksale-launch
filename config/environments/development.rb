2a5feaa0b8b9369239eacb587ec616998bd59a7c
sfelsey <sfelsey@gmail.com>
2014-09-10 23:42:40 -0600

    Added stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,9 +21,6 @@ Prelaunchr::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
@@ -46,6 +43,17 @@ Prelaunchr::Application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { host: 'www.thetrunksale.com', only_path: false }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings   = {
+    address:               "smtp.sendgrid.net",
+    port:                  587,
+    domain:                "thetrunksale.com",
+    user_name:             "TheTrunksale",
+    password:              "Bubbagump101",
+    authentication:        :plain,
+    enable_startttls_auto: true
+  }
+
 end
