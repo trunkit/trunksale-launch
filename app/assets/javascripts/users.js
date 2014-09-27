542a21b21de68299a3e7aa7312e758f858473b94
ian-husted <ian@trunkit.com>
2014-09-26 18:20:32 -0600

    next attempt to make GA events work

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -14,18 +14,45 @@ $(document).ready(function(){
     $('li.product:last-child > .circle').replaceWith("?");*/
 
     //GA email click event
-    $('.emailShare').click(function() {
-        ga('send', 'event', 'Sharing', 'click', 'email');
+    $("a.emailShare").each(function() {
+        var href = $(this).attr("href");
+        var target = $(this).attr("target");
+        var text = $(this).text();
+        $(this).click(function(event) { // when someone clicks these links
+            event.preventDefault(); // don't open the link yet
+            ga('send', 'event', 'Sharing', 'click', 'email'); // create a custom event
+            setTimeout(function() { // now wait 300 milliseconds...
+                window.open(href,(!target?"_self":target)); // ...and open the link as usual
+            },300);
+        });
     });
 
     //GA twitter click event
-    $('.twit').click(function() {
-        ga('send', 'event', 'Sharing', 'click', 'twitter');
+    $("a.twit").each(function() {
+        var href = $(this).attr("href");
+        var target = $(this).attr("target");
+        var text = $(this).text();
+        $(this).click(function(event) { // when someone clicks these links
+            event.preventDefault(); // don't open the link yet
+            ga('send', 'event', 'Sharing', 'click', 'twitter'); // create a custom event
+            setTimeout(function() { // now wait 300 milliseconds...
+                window.open(href,(!target?"_self":target)); // ...and open the link as usual
+            },300);
+        });
     });
 
     //GA facebook click event
-    $('.fb').click(function() {
-        ga('send', 'event', 'Sharing', 'click', 'facebook');
+    $("a.fb").each(function() {
+        var href = $(this).attr("href");
+        var target = $(this).attr("target");
+        var text = $(this).text();
+        $(this).click(function(event) { // when someone clicks these links
+            event.preventDefault(); // don't open the link yet
+            ga('send', 'event', 'Sharing', 'click', 'facebook'); // create a custom event
+            setTimeout(function() { // now wait 300 milliseconds...
+                window.open(href,(!target?"_self":target)); // ...and open the link as usual
+            },300);
+        });
     });
 
 })
